6d0eb15ad9c35992725e31f2bc7d7c6d8370a089
Leo HC Li <36619969+Leo6Leo@users.noreply.github.com>
2022-08-20 23:57:32 -0400

    feat: [NavigationBar] design the navbar component

---
+++ src/components/Global/NavigationBar/index.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { Grid, Layout, Menu } from "antd";
+import { LogoutOutlined, MenuOutlined, UserOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import NavbarLogo from "../../../assets/navbarLogo.png";
+import "./styles.css";
+
+const leftItems = [];
+const rightItems = [
+  { name: "Home", link: "/" },
+  { name: "Map", link: "https://ececourses.ece.utoronto.ca", newTab: true },
+  {
+    name: "Observation",
+    link: "https://ececourses.ece.utoronto.ca",
+    newTab: true,
+  },
+  { name: "About", link: "/About" },
+  { name: "", link: "/login", icon: <UserOutlined style={{ fontSize: 20 }} /> },
+];
+
+const menuItem = (i) => (
+  <Menu.Item key={i.name}>
+    {i.newTab ? (
+      <a href={i.link} target={"_blank"} rel={"noreferrer"}>
+        {i.name}
+      </a>
+    ) : (
+      <Link to={i.link}>
+        {i.icon} {i.name}
+      </Link>
+    )}
+  </Menu.Item>
+);
+
+const NavigationBar = () => {
+  const mobile = !Grid.useBreakpoint()["lg"];
+  const menuItems = (
+    <>
+      {leftItems.map(menuItem)}
+      <Menu.SubMenu
+        key={"navbar-divider"}
+        style={{ flexGrow: 1, visibility: "hidden" }}
+      />
+      {rightItems.map(menuItem)}
+    </>
+  );
+
+  return (
+    <Layout>
+      <Layout.Header>
+        <Menu
+          mode={"horizontal"}
+          theme={"dark"}
+          disabledOverflow={true}
+          style={{ display: "flex" }}
+        >
+          <Menu.Item
+            style={{ flexGrow: mobile && 1, background: "rgba(0,0,0,0)" }}
+            key={"Home"}
+          >
+            <Link to={"/"}>
+              <div id={"navbar-item-elcano-container"}>
+                <img width={110} src={NavbarLogo} alt="logo" />
+                <div id="navbar-item-elcano-text">Climate Lab @ UTSC</div>
+              </div>
+            </Link>
+          </Menu.Item>
+          {mobile ? (
+            <Menu.SubMenu
+              key={"MobileMenu"}
+              title={""}
+              icon={<MenuOutlined />}
+              children={menuItems}
+            />
+          ) : (
+            menuItems
+          )}
+        </Menu>
+      </Layout.Header>
+      <Layout.Content>
+        <br />
+      </Layout.Content>
+    </Layout>
+  );
+};
+
+export default NavigationBar;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import "antd/dist/antd.less";
+import "antd/dist/antd.variable.min.css";
+import NavigationBar from "./components/Global/NavigationBar";
+ReactDOM.render(
+  <div>
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<App />} exact />
+      </Routes>
+    </Router>
+  </div>,
+  document.getElementById("root")
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
