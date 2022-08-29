26792ac5f582fa9c9f26436b7ba0685a1a61b446
Leo HC Li <36619969+Leo6Leo@users.noreply.github.com>
2022-08-29 16:09:44 -0400

    feat: add the welcome banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import CarouselHome from "./components/CarouselHome";
-
+import WelcomeBanner from "./components/Home/WelcomeBanner";
 function App() {
   return (
     <div className="App">
       <CarouselHome />
+      <WelcomeBanner />
     </div>
   );
 }
