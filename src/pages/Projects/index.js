1a45e416c22915bc6c314ab0171514ef2f61adf2
Yukari Hori <78555640+yukarihh@users.noreply.github.com>
2022-10-07 16:07:16 -0400

    Update: Project page

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,31 +1,35 @@
 import bgImage from "../../assets/homebg_3.png";
+import ProjectFirst from "../../assets/smartphone_icon_colour.png";
+import ProjectSecond from "../../assets/online_survey_icon.png";
+import ProjectThird from "../../assets/ice_road_icon.png";
+
 import CoverBanner from "../../components/Global/CoverBanner";
 import ProjectBanner from "../../components/Projects/ProjectBanner";
 function Projects() {
   const BannerContents = [
     {
       id: 1,
-      image: bgImage,
-      title: "ssClimate Change Impacts on Winter Roads and Trails",
+      image: ProjectFirst,
+      title: "Winter Road and Trail Watch",
       content:
-        "Winter road networks are critical land transportation for remote Northern communities who rely on these networks for receiving essential goods and services during winter season. The public and private transportation sectors also rely on winter road networks for their operations. A warming climate has created a shorter winter road season, unreliable road conditions, and safety concerns. ",
+        "We invite all the citizen scientists in the North to contribute to our real-time science research toward climate change adaptation. More details coming soon!",
       bgColor: "white",
     },
 
     {
       id: 2,
-      image: bgImage,
-      title: "Climate Change Impacts on Northern Transportation Networks ",
+      image: ProjectSecond,
+      title: "Winter Road and Trail Community Online Survey",
       content:
-        "In the North, winter roads provide relatively inexpensive overland connection to the all-season road network, which in turn connects from remote to larger communities and the rest of Canada, much passenger and freight movements for remote Northern communities and resource sites occur during the winter road season. Climate change is impacting the surface quality of the winter roads, seasonal lengths, and freight schedule such as the movements of goods and services. ",
+        "We will invite the Winter Road and Trail Watch observers to complete an online survey about climate change impacts on the winter road and trail networks in the Northwest Territories, Yukon, and Nunavut. More details coming soon!",
       bgColor: "#F7FAEF",
     },
     {
       id: 3,
-      image: bgImage,
-      title: "Yukari ",
+      image: ProjectThird,
+      title: "Winter Road Profile",
       content:
-        "A shorter winter road season with less reliable winter road conditions has a substantial socio-economic impact on Northern communities, particularly for remote Indigenous communities. The lack of winter road access for these remote communities has important consequences from a socio-economic perspective, for example, an increase in price of goods and services, which are already expensive in the North. ",
+        "We will work with a community researcher and/or liaison to create a winter road profile for each winter roads.",
       bgColor: "white",
     },
   ];
