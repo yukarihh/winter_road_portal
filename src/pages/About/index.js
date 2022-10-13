b67f32299fd9cbb28eb13ad932caadf0609dae36
Yukari Hori <78555640+yukarihh@users.noreply.github.com>
2022-10-13 14:05:19 -0400

    Update: About page
    UBC

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -6,6 +6,9 @@ import Leo from "../../assets/Leo.png";
 import Haowen from "../../assets/Haowen.png";
 import Larissa from "../../assets/Larissa.png";
 import Amy from "../../assets/Amy.png";
+import Moein from "../../assets/Moein.png";
+import Vivienne from "../../assets/Vivienne.png";
+
 import CoverBanner from "../../components/Global/CoverBanner";
 import Team from "../../components/Team";
 function About() {
@@ -14,7 +17,7 @@ function About() {
       id: 1,
       image: Bill,
       name: "Dr. William A. Gough",
-      title: "Professor",
+      title: "Professor, Physical & Environmental Sciences",
       email: "william.gough@utoronto.ca"
 
     },
@@ -22,7 +25,7 @@ function About() {
         id: 2,
         image: Yukari,
         name: "Dr. Yukari Hori",
-        title: "Research Associate, Project Manager",
+        title: "Research Associate, Physical & Environmental Sciences, Project Manager",
         email: "y.hori@utoronto.ca"
 
       },
@@ -30,7 +33,7 @@ function About() {
         id: 3,
         image: Len,
         name: "Dr. Leonard J.S. Tsuji",
-        title: "Professor",
+        title: "Professor, Physical & Environmental Sciences",
         email: "leonard.tsuji@utoronto.ca"
 
       },
@@ -52,7 +55,7 @@ function About() {
         id: 6,
         image: Larissa,
         name: "Larissa Pizzolato",
-        title: "PhD Student",
+        title: "PhD Student, Physical & Environmental Sciences, Research Assistant",
         email: "larissa.pizzolato@mail.utoronto.ca"
          }
        ], [
@@ -60,26 +63,35 @@ function About() {
           id: 1,
           image: Amy,
           name: "Dr. Amy M. Kim",
-          title: "Associate Professor",
+          title: "Associate Professor, Transportation Engineering",
           email: "amykim@civil.ubc.ca"
     
         },
         {
             id: 2,
             image: bgImage,
-            name: "Leo HC Li",
-            title: "Founder & CEO",
-            email: "leeoo.li@mail.utoronto.ca"
+            name: "Dr. Thomas Stringer",
+            title: "Postdoctoral Fellow, Transportation Engineering",
+            email: "thomas.stringer@ubc.ca"
     
           },
           {
             id: 3,
-            image: bgImage,
-            name: "Leo HC Li",
-            title: "Founder & CEO",
-            email: "leeoo.li@mail.utoronto.ca"
+            image: Moein,
+            name: "Moein Sadeghi",
+            title: "PhD Student, Transportation Engineering",
+            email: "moein.sadeghi@ubc.ca"
     
-          }]
+          },
+            {
+            id: 4,
+            image: Vivienne,
+            name: "Vivienne Li",
+            title: "MSc, Transportation Engineering",
+            email: "vivienne.li@ubc.ca"
+    
+            }
+        ]
 
   ];
   return (
